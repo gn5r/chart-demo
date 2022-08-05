f56d8a2ec3c10ba81a8579d32400f65a832133f4
gn5r <shangyuan.tuolang@gmail.com>
2022-08-05 23:37:16 +0900

    App.vueにSideNavigationとRadarChartを描画

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,60 @@
 <template>
   <v-app>
-    <router-view />
+    <v-main>
+      <div class="d-flex" style="height: 100%">
+        <side-navigation v-model="component" ref="nav" :items="items" />
+        <component :is="component" :style="contentsStyle" />
+      </div>
+    </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { Navigation } from "types";
+import SideNavigation from "@/components/SideNavigation.vue";
+import RadarChart from "@/views/RadarChart.vue";
 
 export default Vue.extend({
   name: "app",
   model: {},
   mixins: [],
   props: {},
-  data: () => ({}),
+  data: () => ({
+    component: "radar-chart",
+    items: [
+      {
+        title: "Samples",
+        group: true,
+        items: [
+          {
+            icon: "mdi-hexagon-outline",
+            title: "Radar Chart",
+            component: "radar-chart",
+          },
+        ],
+      },
+    ] as Array<Navigation>,
+  }),
   methods: {},
   created() {
     //
   },
-  computed: {},
+  computed: {
+    contentsStyle() {
+      const { nav } = this.$refs;
+      const navWidth = nav !== undefined ? (nav as any).width : "0px";
+      return {
+        height: "100%",
+        width: `calc(100% - ${navWidth})`,
+      };
+    },
+  },
   watch: {},
-  components: {},
+  components: {
+    SideNavigation,
+    RadarChart,
+  },
 });
 </script>
 
---
+++ src/views/RadarChart.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-card height="100%" width="100%"></v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "radar-chart",
+  model: {},
+  mixins: [],
+  props: {},
+  data: () => ({}),
+  methods: {},
+  created() {
+    //
+  },
+  computed: {},
+  watch: {},
+  components: {},
+});
+</script>
+
+<style scoped></style>
