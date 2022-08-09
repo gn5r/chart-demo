17357676ffec7ab457130c4240331f3e332e97b9
gn5r <shangyuan.tuolang@gmail.com>
2022-08-09 21:38:43 +0900

    build: vue-chartjsとchart.jsをダウングレード、vue-loadign-overlay v3を追加

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@/assets/sass/chart-demo.sass";
+import "vue-loading-overlay/dist/vue-loading.css";
+import VLoading from "vue-loading-overlay";
 
 Vue.config.productionTip = false;
+
+Vue.component("VLoading", VLoading);
 
 new Vue({
   vuetify,
